5ff785b3841a7b0da2c77bc161fc151d3b6fcf28
normen-zzz <normanardian24@gmail.com>
2020-12-16 18:08:14 +0700

    Update Notification.php

--- application/controllers/Notification.php
+++ application/controllers/Notification.php
@@ -31,9 +31,6 @@ class Notification extends CI_Controller {
 
 	public function index()
 	{
-		echo 'test notification handler';
-		$json_result = file_get_contents('php://input');
-		$result = json_decode($json_result);
 
 		 echo 'test notification handler';
         $json_result = file_get_contents('php://input');
